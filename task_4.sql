51a46aedbc3bfa94f8bce72d44a83a0cacb12e56
farouk <noblefarz7@gmail.com>
2025-12-14 23:44:39 +0100

    task_4

--- task_4.sql
+++ task_4.sql
@@ -1,8 +1,11 @@
+
+
+
 -- task_4.sql
--- Prints full description of the table books without using DESCRIBE or EXPLAIN
+-- Prints full description of the table Books without using DESCRIBE or EXPLAIN
 
 USE alx_book_store;
 
 SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
 FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'Books';
+WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'Books';
